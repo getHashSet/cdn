05af2fae76b9c1e88c341c9780de4802af6e898d
Matthew Carpenter <53285976+getHashSet@users.noreply.github.com>
2020-02-12 19:11:10 -0700

    Add files via upload

---
+++ liz_javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Thank you for visiting LizLandeen.com");
+
+    document.addEventListener("click", function(event) {
+        console.log(event);
+    });
+});
